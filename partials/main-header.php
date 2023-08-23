a328928c563981e468f4c014afcd8a8e4a41440a
geomarts <netdark@hotmail.gr>
2023-08-23 14:04:56 +0300

    add logo link

--- partials/main-header.php
+++ partials/main-header.php
@@ -1,6 +1,6 @@
 <header class="site-header">
 	<div class="container">
-		<a href="">
+		<a href="<?php echo esc_url( home_url( '/' ) ); ?>">
 			<img width="178" height="38" src="https://assets.codepen.io/162656/forecastr-logo-white.svg" alt="forecastr logo">
 		</a>
 	</div>
